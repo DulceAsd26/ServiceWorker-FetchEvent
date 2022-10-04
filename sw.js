781bd56dc59d175d9fdf4f05d226cb7f5b4ca691
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 21:52:47 -0500

    Modificando la respuesta de petición Fetch

--- sw.js
+++ sw.js
@@ -5,14 +5,20 @@
 self.addEventListener('fetch', event => {
 
 
-    if(event.request.url.includes('.jpg') ){
-        console.log(event.request.url);
+    if( event.request.url.includes('style.css') ) {
 
-        //let fotoReq = fetch('img/main.jpg');
-        //let fotoReq = fetch(event.request.url);
-        let fotoReq = fetch(event.request);
+        let respuesta = new Response(`
+        body {
+            background-color: red !important;
+            color: pink;
+        }
+        `, {
+            headers: {
+                'Content-Type': 'text/css'
+            }
+        });
 
-        event.respondWith( fotoReq);
+        event.respondWith( respuesta);
     }
  
 
