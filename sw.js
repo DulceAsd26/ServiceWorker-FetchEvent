adab9f3c3d30a5bfa80d1f24b9c692b47e9631a4
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 21:13:51 -0500

    Fetch Event

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 
 
-console.log('SW: Hola mundo');
+
+
+self.addEventListener('fetch', event => {
+
+    if( event.request.url.includes('style.css') ){
+        event.respondWith( null );
+    }else{
+        event.respondWith( fetch( event.request ) );
+    }
+
+ 
+
+    
+});
