30fe32549680b4ee45e6a633a5fd50bef9ae6b8d
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 21:29:02 -0500

    Peticiones desde el evento Fetch

--- sw.js
+++ sw.js
@@ -4,12 +4,16 @@
 
 self.addEventListener('fetch', event => {
 
-    if( event.request.url.includes('style.css') ){
-        event.respondWith( null );
-    }else{
-        event.respondWith( fetch( event.request ) );
-    }
 
+    if(event.request.url.includes('.jpg') ){
+        console.log(event.request.url);
+
+        //let fotoReq = fetch('img/main.jpg');
+        //let fotoReq = fetch(event.request.url);
+        let fotoReq = fetch(event.request);
+
+        event.respondWith( fotoReq);
+    }
  
 
     
