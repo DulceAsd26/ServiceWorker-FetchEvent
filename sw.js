360b6a5bda3ef804333a8e019f0032c64ff67588
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-03 22:04:51 -0500

    Interceptando y modificando peticiones

--- sw.js
+++ sw.js
@@ -5,21 +5,11 @@
 self.addEventListener('fetch', event => {
 
 
-    if( event.request.url.includes('style.css') ) {
+   if( event.request.url.includes('main.jpg') ){
 
-        let respuesta = new Response(`
-        body {
-            background-color: red !important;
-            color: pink;
-        }
-        `, {
-            headers: {
-                'Content-Type': 'text/css'
-            }
-        });
+    event.respondWith( fetch('img/main-patas-arriba.jpg') );
 
-        event.respondWith( respuesta);
-    }
+   }
  
 
     
